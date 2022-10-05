e434c8cfd91f31e3278ae11eb64f04dc5826a30b
sandip suryawanshi <suryawanshisandip299@gmail.com>
2022-10-05 09:43:12 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Home from "./src/Components/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Text>hello sandip!</Text>
+      <Image
+        style={styles.Imagestyle}
+        source={{
+          uri: "https://images.unsplash.com/photo-1562690868-60bbe7293e94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cm9zZSUyMGZsb3dlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
+        }}
+      />
+
       <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
@@ -13,8 +22,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Imagestyle: {
+    width: 200,
+    height: 200,
   },
 });
